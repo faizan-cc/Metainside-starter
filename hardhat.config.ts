ce34a15a776738cac6a3460ccebcbdc9682aed65
faizan.rizwan <faizan.rizwan@rnssol.com>
2025-09-15 19:14:27 +0500

    first commit

---
+++ hardhat.config.ts
@@ -0,0 +1,55 @@
+import type { HardhatUserConfig } from "hardhat/config";
+
+import hardhatToolboxViemPlugin from "@nomicfoundation/hardhat-toolbox-viem";
+import { SensitiveString } from "hardhat/types/config";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+console.log(`process.env.BASESCAN_API_KEY`);
+console.log(process.env.BASESCAN_API_KEY);
+console.log(`process.env.BASESCAN_API_KEY`);
+
+const config: HardhatUserConfig = {
+  plugins: [hardhatToolboxViemPlugin],
+  verify: {
+    etherscan: {
+      apiKey: process.env.BASESCAN_API_KEY as SensitiveString,
+      // customChains: [
+      //   {
+      //     network: "baseSepolia",
+      //     chainId: 84532,
+      //     urls: {
+      //       apiURL: "https://api-sepolia.basescan.org",
+      //       browserURL: "https://sepolia.basescan.org",
+      //     },
+      //   },
+      // ],
+    },
+  },
+  solidity: {
+    profiles: {
+      default: {
+        version: "0.8.28",
+      },
+      production: {
+        version: "0.8.28",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    },
+  },
+  networks: {
+    baseSepolia: {
+      type: "http",
+      chainType: "op",
+      url: process.env.SEPOLIA_RPC_URL as SensitiveString,
+      accounts: [process.env.PRIVATE_KEY as SensitiveString],
+    },
+  },
+};
+
+export default config;
